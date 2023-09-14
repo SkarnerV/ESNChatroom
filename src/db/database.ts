585aefc79cd661f1c081e2f3f084f9ce65e6e130
ZiyueWang675 <ziyue_wang@ucsb.edu>
2023-09-13 17:19:38 -0700

    changed_mongoKey_To_env

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,17 +1,17 @@
 import mongoose, { ConnectOptions } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 class Database {
   private static instance: Database;
   private mongoDB: any;
 
   constructor() {
-    mongoose.connect(
-      "mongodb+srv://sb1:u3PjrHNCr5gTKasH@esn.pwj9fb8.mongodb.net/",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      } as ConnectOptions
-    );
+    const mongodbKey = process.env.MONGODB_KEY || "";
+    mongoose.connect(mongodbKey, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
 
     this.mongoDB = mongoose;
   }
