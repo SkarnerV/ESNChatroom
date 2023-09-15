956c7d423cbe8e0b6b98b6106f58e96cf7bf5f86
yuw7F2023 <yuw7@andrew.cmu.edu>
2023-09-15 12:25:34 -0700

    separate the register funtion to another Router class

--- index.ts
+++ index.ts
@@ -2,19 +2,18 @@ import express, { Router, Request, Response } from "express";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 import bodyParser from "body-parser";
-import userDB from "./src/db/user";
+import userRouter from "./src/userRouter";
 
 class App {
   private app: express.Application;
   private server: any;
-  private router: Router;
-  private users: userDB;
+  private router: userRouter;
+
 
   constructor() {
     this.app = express();
     this.server = createServer(this.app);
-    this.router = express.Router();
-    this.users = new userDB();
+    this.router = new userRouter();
   }
 
   private registerPortListener(): void {
@@ -27,25 +26,10 @@ class App {
   private reroute(): void {
     this.app.use(express.static(__dirname + '/public'));
     this.app.get("/");
+    
+    // Handles register action
+    this.app.post('/auth/register', this.router.register);
 
-    // Handles 
-    this.app.post('/register', async (req: Request, res: Response) => {
-        // Here password will need to add encryption
-        const { username, password } = req.body;
-        if(!username || !password) {
-            res.status(400).json({message: "Username and Password are required"});
-        }
-        if(this.users.findUser(username.toLowerCase()) != null){
-            res.status(400).json({message: "Username already exist"});
-        }
-        try {
-            this.users.createUser(username.toLowerCase(), password);
-            res.status(201).json({message: "User registered complete"});
-        } catch (error){
-            res.status(500).json({ message: 'Server error' });
-        }
-        
-    });
   }
 
   start(): void {
---
+++ src/userRouter.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import userDB from "./db/user";
+
+class userRouter {
+    private users: userDB;
+
+    //Initiate the DB
+    constructor(){
+        this.users = new userDB();
+    }
+
+
+    /**** 
+    This function has the features:
+      - Check if both username and password field are non-empty
+      - Find if a user is already in the db (using all lower case)
+      - If it is a new user add the username(all lower case) along with password to the db
+      ## Each action will return a message with json to the client to indicate the status
+    ****/
+    async register (req: Request, res: Response): Promise<void> {
+        // Here password will need to add encryption
+        const { username, password } = req.body;
+        if(!username || !password) {
+            res.status(400).json({message: "Username and Password are required"});
+        }
+        if(this.users.findUser(username.toLowerCase()) != null){
+            res.status(400).json({message: "Username already exist"});
+        }
+        try {
+            this.users.createUser(username.toLowerCase(), password);
+            res.status(201).json({message: "User registered complete"});
+        } catch (error){
+            res.status(500).json({ message: 'Server error' });
+        }
+        
+    };
+}
+
+export default userRouter;
