62a635fd6b75d2777baba3690f1756aa2411fb55
shuhanghS2023 <shuhang.han@andrew.cmu.edu>
2023-09-15 15:19:52 -0700

    eslint config

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+const config = {
+    trailingComma: 'all',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    proseWrap: 'always',
+    htmlWhitespaceSensitivity: 'ignore',
+    endOfLine: 'lf',
+    quoteProps: 'consistent',
+}
+
+module.exports = config
